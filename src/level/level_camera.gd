eed4eb5f485d0b65eca7c2ef782a816b3968a0ea
Juan Gaspar García <24842781+PKGaspi@users.noreply.github.com>
2024-05-27 20:10:11 +0200

    Zoom to cursor

--- src/level/level_camera.gd
+++ src/level/level_camera.gd
@@ -9,6 +9,9 @@ const ZOOM_STEP: Vector2 = Vector2(.1, .1)
 const ZOOM_MAX: Vector2 = Vector2(3, 3)
 const ZOOM_MIN: Vector2 = Vector2(.4, .4)
 
+func _ready() -> void:
+	global_position = get_tree().root.size / 2
+
 func _input(event):
 	# Source: https://forum.godotengine.org/t/how-to-drag-camera-with-mouse/28508/2
 	if event.is_action("drag"):
@@ -20,10 +23,14 @@ func _input(event):
 			dragging = false
 	elif event is InputEventMouseMotion and dragging:
 		position = (mouse_start_pos - event.position) / zoom + screen_start_position
-	elif not dragging and event.is_action("zoom_in"):
-		_set_zoom(zoom + ZOOM_STEP)
-	elif not dragging and event.is_action("zoom_out"):
-		_set_zoom(zoom - ZOOM_STEP)
+	elif not dragging and event.is_action_pressed("zoom_in"):
+		zoom_at_mouse(zoom + ZOOM_STEP)
+	elif not dragging and event.is_action_pressed("zoom_out"):
+		zoom_at_mouse(zoom - ZOOM_STEP)
 
-func _set_zoom(value: Vector2) -> void:
+	
+func zoom_at_mouse(value: Vector2) -> void:
+	var mouse_pos_1: Vector2 = get_global_mouse_position()
 	zoom = clamp(value, ZOOM_MIN, ZOOM_MAX)
+	var mouse_pos_2: Vector2 = get_global_mouse_position()
+	global_position += mouse_pos_1 - mouse_pos_2
