674db32da1aafe1c1342a5d181e1daf2ff9be60a
Juan Gaspar García <24842781+PKGaspi@users.noreply.github.com>
2024-06-01 19:54:16 +0200

    More theme and snappy eyes

--- src/level/building/eyeball.gd
+++ src/level/building/eyeball.gd
@@ -2,7 +2,7 @@ extends Sprite2D
 
 
 var movement_range: Vector2 = Vector2(8, 3)
-var distance_range: Vector2 = Vector2(20, 120)
+var distance_range: Vector2 = Vector2(8, 120)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -14,3 +14,4 @@ func _physics_process(_delta: float) -> void:
 	var global_mouse_position: Vector2 = get_global_mouse_position()
 	var distance: float = global_position.distance_to(global_mouse_position)
 	position = global_position.direction_to(global_mouse_position) * lerp(Vector2.ZERO, movement_range, clamp((distance-distance_range.x)/distance_range.y, 0, 1))
+	position = round(position)
